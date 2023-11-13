9ea55eb84f8f664755899c34364ab62f606d7b8e
jbredwards <143640517+JulianUSM@users.noreply.github.com>
2023-11-13 17:28:09 -0500

    push unfinished split method

--- Lab8/insertTTF.java
+++ Lab8/insertTTF.java
@@ -46,6 +46,34 @@ public class insertTTF<E extends Comparable<E>> extends TwoThreeFourTree<E>
     	}
     }
 
+	protected void split(final Node<E> nodeParent, final Node<E> nodeToSplit, final E data) {
+		final int splitIndex = order >>> 1;
+		final Node<E> newLocalRoot = new Node<>(order);
+		newLocalRoot.data[splitIndex] = nodeToSplit.data[splitIndex];
+
+		final Node<E> leftChild = new Node<>(order);
+		leftChild.size = splitIndex;
+		System.arraycopy(nodeToSplit.data, 0, leftChild.data, 0, splitIndex);
+
+		final Node<E> rightChild = new Node<>(order);
+		rightChild.size = nodeToSplit.size - splitIndex;
+
+		// split root
+		if(nodeParent == null) {
+
+		}
+		// split normal node
+		else {
+
+		}
+
+       	// final Node<E> left = new Node<>(order);
+        // System.arraycopy(nodeToSplit.data, 0, left.data, 0, splitIndex);
+        // System.arraycopy(nodeToSplit.children, 0, left.children, 0, splitIndex - 1);
+        // final Node<E> right = new Node<>(order);
+        // if(nodeToSplit.data.length - (splitIndex + 1) >= 0) System.arraycopy(nodeToSplit.data, splitIndex + 1, right.data, splitIndex + 1, nodeToSplit.data.length - (splitIndex + 1));
+	}
+
     /**
      * @param data
      * @return
