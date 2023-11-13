d5e072b1b7f413c8089ec982951106015a81e8a4
Cleeps <89921789+Cleeps@users.noreply.github.com>
2023-11-13 14:50:45 -0500

    Final commit

--- Lab8/insertTTF.java
+++ Lab8/insertTTF.java
@@ -3,7 +3,7 @@ package Lab8;
 /**
  *
  * @since 13 November 2023
- * @author Julian Edwards and
+ * @author Julian Edwards and Colby Cormier
  *
  */
 public class insertTTF<E extends Comparable<E>> extends TwoThreeFourTree<E>
@@ -13,7 +13,37 @@ public class insertTTF<E extends Comparable<E>> extends TwoThreeFourTree<E>
      */
     @Override
     public void insert(final E data) {
-
+    	
+    	// Base Case
+    	if(root == null || root.children.length == 0)
+    	{
+    		return;
+    	}
+    	
+    	// Recursive cases
+    	switch(root.children.length)
+    	{
+	    	case 2:
+	    		
+	    		if(root.children[0].data[0].compareTo(data) < 0)
+	    		{
+	    			root.children[] = insert();
+	    		} else if(root.children[1].data[0].compareTo(data) > 0) { 
+	    			
+	    		}
+	    		
+	    		break;
+	    	case 3:
+	    		break;
+	    		
+	    	// Split when encountering a 4-node
+	    	case 4:
+	    		split();
+	    		break;
+	    		
+    		default:
+    			break;
+    	}
     }
 
     /**
